26af1d69ec20f24a522d7ae0e20c7e2d0aa4c894
邱達儒 <1234567si2k@gmail.com>
2024-07-15 14:35:28 +0800

    1

--- 2024暑假一周/ordering system.py
+++ 2024暑假一周/ordering system.py
@@ -1,12 +1,51 @@
 #menu_items = ["pizza","steak","pasta"]
 #menu_items_price = [11,30,15]
-ingredients = {"slice of bread":10,"cheese":10,"lettuce":5, "tomato":10, "ham":15, "mayo":5, "peanut butter":5, "strawberry jam": 6, "chocolate syrup":6, "honey":6, "bean sprout":10, "beef":20, "egg":12, "bell pepper":10, "red beans":10, "mochi":30, "strawberries":}
-
-user_choice = list(map(str,input().split()))
+ingredients = {"slice of bread":10,"cheese":10,"lettuce":5, "tomato":10, "ham":15, "mayo":5, "peanut butter":5, "strawberry jam": 6, "chocolate syrup":6, "honey":6, "bean sprout":10, "beef":20, "egg":12, "bell pepper":10, "red beans":10, "mochi":30, "strawberries":20}
 spend = 0
-for i in user_choice:
-        spend += menu_items[i]
-print(spend)
+user_choice = input("what do you need ? choose one number\n 1.print menu \n 2.order your sandwich \n 3.Enter a price and I'll make you a sandwich : ")
+
+def choice1(ingredients1):
+        for ingredient, price in ingredients1.items():
+                print(f"{ingredient}: ${price}")
+
+
+
+
+
+def choice2(ingredients):
+        user_choice=list(map(str,input("order your sandwich : ").split()))
+        spend = 0
+        for i in user_choice:
+                spend += ingredients[i]
+        print(spend)
+
+def choice3(ingredients,cost):
+        for i in range(ingredients.values()):
+                for i1 in range(ingredients.values()):
+                        for i2 in range(3):
+                                if i+i1 == cost:
+                                        
+
+
+
+        
+
+
+if user_choice == "1":
+        choice1(ingredients)
+if user_choice == "2":
+        choice2(ingredients)
+if user_choice == "3":
+        cost = input("cost : ")
+        choice3(ingredients,cost)
+
+
+#for i in user_choice:
+ #       spend += ingredients[i]
+
+
+
+#print(spend)
 
 
 
---
+++ 2024暑假一周/計算機.py
@@ -0,0 +1,16 @@
+#  print
+#  input
+#  while
+#  for
+#  if
+
+#  x = int(input())
+#  print("x = ",x,"x+x=", x+x)
+
+
+# 1+....100 = ?
+ans = 0
+for i in range(1,101):
+    ans = ans + i # ans += i     ans = ans+i
+print(ans)
+
